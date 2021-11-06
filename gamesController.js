979c75d248e29e120c22c71b893a11c08b0f6bea
VincentiusT <tb.vincentius@gmail.com>
2021-11-06 13:57:30 +0700

    update games page

---
+++ gamesController.js
@@ -0,0 +1,41 @@
+function showDiv(id) {
+    document.getElementById(id).style.display = "block";
+}
+ 
+function hideDiv(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function hideAll(){
+    document.getElementById("bounceballDiv").style.display = "none";
+    document.getElementById("24Div").style.display = "none";
+}
+
+ var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n, id, dotid) {
+  showSlides(slideIndex += n, id, dotid);
+}
+
+// Thumbnail image controls
+function currentSlide(n, id, dotid) {
+  showSlides(slideIndex = n, id, dotid);
+}
+
+function showSlides(n, id, dotid) {
+  var i;
+  var slides = document.getElementsByClassName(id);
+  var dots = document.getElementsByClassName(dotid);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
